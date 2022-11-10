a6d2fafd490ca3d56e5303dc8d0a12ef89ec016a
Maticraft <mati18a@poczta.fm>
2022-11-10 08:52:36 +0100

    updated after lab 5

--- labs4/complex.py
+++ labs4/complex.py
@@ -6,14 +6,30 @@ a także udostępniać metody, do odczytywania i nadawania ich wartości. Ponadt
 powinna implementować metody do obliczania modułu i argumentu liczby zespolonej.
 Zademonstruj działanie klasy w programie.
 """
+"""
+__add__(self, other),
+• __radd__(self, other),
+• __iadd__(self, other),
+• __sub__(self, other),
+• __rsub__(self, other),
+• __isub__(self, other),
+• __mul__(self, other),
+• __rmul__(self, other),
+• __imul__(self, other),
+• __truediv__(self, other),
+• __rtruediv__(self, other),
+• __itruediv__(self, other),
+• __neg__(self),
+• __abs__(self).
+"""
 
 import math
 
 
 class Complex():
-    def __init__(self):
-        self.__real = 0
-        self.__imaginary = 0
+    def __init__(self, x=0, y=0):
+        self.__real = x
+        self.__imaginary = y
 
     def set_real(self, real):
         self.__real = real
@@ -32,3 +48,95 @@ class Complex():
         
     def argument(self):
         return math.acos(self.__real / self.module())
+
+    def __str__(self) -> str:
+        return str((self.__real, self.__imaginary))
+
+    def __add__(self, other):
+        if type(other) == Complex:
+            x = other.get_real() + self.__real
+            y = other.get_imaginary() + self.__imaginary
+            return Complex(x, y)
+        else:
+            raise NotImplementedError
+
+    def __radd__(self, other):
+        if type(other) == Complex:
+            return self.__add__(other)
+        else:
+            raise NotImplementedError
+
+    def __iadd__(self, other):
+        if type(other) == Complex:
+            self.__real += other.get_real()
+            self.__imaginary += other.get_imaginary()
+            return self
+        else:
+            raise NotImplementedError
+        
+    def __neg__(self):
+        return Complex(-self.__real, -self.__imaginary)
+
+    def __sub__(self, other):
+        if type(other) == Complex:
+            return self.__add__(-other)
+        else:
+            raise NotImplementedError
+
+    def __rsub__(self, other):
+        if type(other) == Complex:
+            return other.__sub__(self)
+        else:
+            raise NotImplementedError
+
+    def __isub__(self, other):
+        if type(other) == Complex:
+            self.__iadd__(other.__neg__())
+            return self
+        else:
+            raise NotImplementedError
+
+    def __mul__(self, other):
+        if type(other) == Complex:
+            x = other.get_real() * self.__real - other.get_imaginary() * self.__imaginary
+            y = other.get_real() * self.__imaginary + other.get_imaginary() * self.__real
+            return Complex(x, y)
+        else:
+            raise NotImplementedError
+
+    def __rmul__(self, other):
+        if type(other) == Complex:
+            return self.__mul__(other)
+        else:
+            raise NotImplementedError
+
+    def __imul__(self, other):
+        if type(other) == Complex:
+            self = self.__mul__(other)
+            return self
+        else:
+            raise NotImplementedError
+
+    def __truediv__(self, other):
+        if type(other) == Complex:
+            x = (other.get_real() * self.__real - other.get_imaginary() * self.__imaginary) / (other.module() ** 2)
+            y = (other.get_real() * self.__imaginary + other.get_imaginary() * self.__real) / (other.module() ** 2)
+            return Complex(x, y)
+        else:
+            raise NotImplementedError
+
+    def __rtruediv__(self, other):
+        if type(other) == Complex:
+            return other.__truediv__(self)
+        else:
+            raise NotImplementedError
+
+    def __itruediv__(self, other):
+        if type(other) == Complex:
+            self = self.__truediv__(other)
+            return self
+        else:
+            raise NotImplementedError
+
+    def __abs__(self):
+        return self.module()
--- labs4/task1.py
+++ labs4/task1.py
@@ -1,16 +1,14 @@
 import complex
 
 def main():
-    c1 = complex.Complex()
-    c2 = complex.Complex()
+    c1 = complex.Complex(3, 4)
+    c2 = complex.Complex(1, 2)
     
-    print(c1.get_real())
-    c1.set_real(4)
-    c1.set_imaginary(5)
-    print(c1.get_real())
-    print(c1.get_imaginary())
-    print(c1.module())
-    print(c1.argument())
+    c3 = c1 / c2
+    c1 /= c2
+    print(c1)
+    print(c3)
+   
 
 if __name__ == '__main__':
     main()
